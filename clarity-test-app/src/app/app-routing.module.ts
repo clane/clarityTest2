6bb3af54f348cf25c330c52198949f2d1b379229
Chris Lane <chlane@chlane-a01.vmware.com>
2019-04-25 12:00:56 -0700

    routing is working

--- clarity-test-app/src/app/app-routing.module.ts
+++ clarity-test-app/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BasicComponent } from './dataGrids/basic/basic.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{ path: 'datagrid-basic', component: BasicComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
