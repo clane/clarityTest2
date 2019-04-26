e50da27e417ee78755ca578709ce9ad24344872b
Chris Lane <chlane@chlane-a01.vmware.com>
2019-04-26 12:02:31 -0700

    single selection working

--- clarity-test-app/src/app/dataGrids/single-selection/single-selection.component.ts
+++ clarity-test-app/src/app/dataGrids/single-selection/single-selection.component.ts
@@ -9,6 +9,15 @@ export class SingleSelectionComponent implements OnInit {
 
   constructor() { }
 
+
+ users = [
+        {id:"1", name:"Kevin", creation:"Mon Apr 22 10:00:51 PDT 2019", color:"blue", pokemon:{"name":"Kabuto","number":1}},
+        {id:"2", name:"Jet", creation:"Mon Apr 22 10:00:51 PDT 2019", color:"red", pokemon:{"name":"Muk","number":2}},
+        {id:"3", name:"Fred", creation:"Mon Apr 22 10:00:51 PDT 2019", color:"green", pokemon:{"name":"Tentacool","number":3}},
+    ];
+
+
+
   ngOnInit() {
   }
 
