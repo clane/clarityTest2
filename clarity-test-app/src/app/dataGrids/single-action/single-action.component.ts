00a1a299c6d37b50f0710f32cf1e48979a8b88d7
Chris Lane <chlane@chlane-a01.vmware.com>
2019-04-26 15:58:25 -0700

    single action demo mostly working

--- clarity-test-app/src/app/dataGrids/single-action/single-action.component.ts
+++ clarity-test-app/src/app/dataGrids/single-action/single-action.component.ts
@@ -9,6 +9,29 @@ export class SingleActionComponent implements OnInit {
 
   constructor() { }
 
+
+users = [
+        {id:"1", name:"Kevin", creation:"Mon Apr 22 10:00:51 PDT 2019", color:"blue", pokemon:{"name":"Kabuto","number":1}},
+        {id:"2", name:"Jet", creation:"Mon Apr 22 10:00:51 PDT 2019", color:"red", pokemon:{"name":"Muk","number":2}},
+        {id:"3", name:"Fred", creation:"Mon Apr 22 10:00:51 PDT 2019", color:"green", pokemon:{"name":"Tentacool","number":3}},
+];
+
+
+    singleCleanUp(): void {
+        this.toDelete = null;
+        this.toEdit = null;
+    }
+
+    singleOnEdit(user: User) {
+        this.toEdit = user;
+    }
+
+    singleOnDelete(user: User) {
+        this.singleToDelete = user;
+    }
+
+
+
   ngOnInit() {
   }
 
