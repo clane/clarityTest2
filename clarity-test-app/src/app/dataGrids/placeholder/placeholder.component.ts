808357bf42e255527cf49bbd8f70dc038cc8741c
Chris Lane <chlane@chlane-a01.vmware.com>
2019-04-28 06:23:26 -0700

    adding placehoder files

---
+++ clarity-test-app/src/app/dataGrids/placeholder/placeholder.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-placeholder',
+  templateUrl: './placeholder.component.html',
+  styleUrls: ['./placeholder.component.css']
+})
+export class PlaceholderComponent implements OnInit {
+
+  constructor() { }
+
+  users = []; 
+
+  ngOnInit() {
+  }
+
+}
