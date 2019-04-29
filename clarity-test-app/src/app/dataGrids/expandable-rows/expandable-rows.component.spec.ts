bbb585919d91440012f056d8c9fb5994ec2c7ad7
Chris Lane <chlane@chlane-a01.vmware.com>
2019-04-29 12:46:59 -0700

    geting started on expandable rows example, routing now works

---
+++ clarity-test-app/src/app/dataGrids/expandable-rows/expandable-rows.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ExpandableRowsComponent } from './expandable-rows.component';
+
+describe('ExpandableRowsComponent', () => {
+  let component: ExpandableRowsComponent;
+  let fixture: ComponentFixture<ExpandableRowsComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ExpandableRowsComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ExpandableRowsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
