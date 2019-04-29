576c9b00e5c172ffed70d1a9d0318c16605da920
Chris Lane <chlane@chlane-a01.vmware.com>
2019-04-29 10:31:45 -0700

    adding visual focus indication for headings and routing,title and focus management now working for the basic example

--- clarity-test-app/src/app/dataGrids/basic/basic.component.ts
+++ clarity-test-app/src/app/dataGrids/basic/basic.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'datagrid-basic',
@@ -7,32 +8,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BasicComponent implements OnInit {
 
-  constructor() { }
-
-
-  users = [
-        {id:"1", name:"Kevin", creation:"Mon Apr 22 10:00:51 PDT 2019", color:"blue", pokemon:{"name":"Kabuto","number":1}},
-        {id:"2", name:"Jet", creation:"Mon Apr 22 10:00:51 PDT 2019", color:"red", pokemon:{"name":"Muk","number":2}},
-        {id:"3", name:"Fred", creation:"Mon Apr 22 10:00:51 PDT 2019", color:"green", pokemon:{"name":"Tentacool","number":3}},
-        {id:"4", name:"Fred", creation:"Mon Apr 22 10:00:51 PDT 2019", color:"green", pokemon:{"name":"Tentacool","number":3}},
-        {id:"5", name:"Fred", creation:"Mon Apr 22 10:00:51 PDT 2019", color:"green", pokemon:{"name":"Tentacool","number":3}},
-        {id:"6", name:"Fred", creation:"Mon Apr 22 10:00:51 PDT 2019", color:"green", pokemon:{"name":"Tentacool","number":3}},
-        {id:"3", name:"Fred", creation:"Mon Apr 22 10:00:51 PDT 2019", color:"green", pokemon:{"name":"Tentacool","number":3}},
-        {id:"3", name:"Fred", creation:"Mon Apr 22 10:00:51 PDT 2019", color:"green", pokemon:{"name":"Tentacool","number":3}},
-        {id:"3", name:"Fred", creation:"Mon Apr 22 10:00:51 PDT 2019", color:"green", pokemon:{"name":"Tentacool","number":3}},
-        {id:"3", name:"Fred", creation:"Mon Apr 22 10:00:51 PDT 2019", color:"green", pokemon:{"name":"Tentacool","number":3}},
-        {id:"3", name:"Fred", creation:"Mon Apr 22 10:00:51 PDT 2019", color:"green", pokemon:{"name":"Tentacool","number":3}},
-        {id:"3", name:"Fred", creation:"Mon Apr 22 10:00:51 PDT 2019", color:"green", pokemon:{"name":"Tentacool","number":3}},
-        {id:"3", name:"Fred", creation:"Mon Apr 22 10:00:51 PDT 2019", color:"green", pokemon:{"name":"Tentacool","number":3}},
-        {id:"3", name:"Fred", creation:"Mon Apr 22 10:00:51 PDT 2019", color:"green", pokemon:{"name":"Tentacool","number":3}},
-        {id:"3", name:"Fred", creation:"Mon Apr 22 10:00:51 PDT 2019", color:"green", pokemon:{"name":"Tentacool","number":3}},
-        {id:"3", name:"Fred", creation:"Mon Apr 22 10:00:51 PDT 2019", color:"green", pokemon:{"name":"Tentacool","number":3}},
-        {id:"3", name:"Fred", creation:"Mon Apr 22 10:00:51 PDT 2019", color:"green", pokemon:{"name":"Tentacool","number":3}},
-        {id:"3", name:"Fred", creation:"Mon Apr 22 10:00:51 PDT 2019", color:"green", pokemon:{"name":"Tentacool","number":3}},
-    ];
-
+  constructor(private titleService: Title) { }
+  componentTitle = 'Data Grid - Basic Structure';
+  setTitle(newTitle: string) { this.titleService.setTitle(newTitle); }
+  @ViewChild('componentHeading') elementToFocusOnInit;
 
   ngOnInit() {
+    this.setTitle(this.componentTitle);
+    this.elementToFocusOnInit.nativeElement.focus();
+    console.log('test');
   }
+
+  users = [
+    { id: "1", name: "Kevin", creation: "Mon Apr 22 10:00:51 PDT 2019", color: "blue", pokemon: { "name": "Kabuto", "number": 1 } },
+    { id: "2", name: "Jet", creation: "Mon Apr 22 10:00:51 PDT 2019", color: "red", pokemon: { "name": "Muk", "number": 2 } },
+    { id: "3", name: "Fred", creation: "Mon Apr 22 10:00:51 PDT 2019", color: "green", pokemon: { "name": "Tentacool", "number": 3 } },
+    { id: "4", name: "Fred", creation: "Mon Apr 22 10:00:51 PDT 2019", color: "green", pokemon: { "name": "Tentacool", "number": 3 } },
+    { id: "5", name: "Fred", creation: "Mon Apr 22 10:00:51 PDT 2019", color: "green", pokemon: { "name": "Tentacool", "number": 3 } },
+    { id: "6", name: "Fred", creation: "Mon Apr 22 10:00:51 PDT 2019", color: "green", pokemon: { "name": "Tentacool", "number": 3 } },
+    { id: "3", name: "Fred", creation: "Mon Apr 22 10:00:51 PDT 2019", color: "green", pokemon: { "name": "Tentacool", "number": 3 } },
+    { id: "3", name: "Fred", creation: "Mon Apr 22 10:00:51 PDT 2019", color: "green", pokemon: { "name": "Tentacool", "number": 3 } },
+    { id: "3", name: "Fred", creation: "Mon Apr 22 10:00:51 PDT 2019", color: "green", pokemon: { "name": "Tentacool", "number": 3 } },
+    { id: "3", name: "Fred", creation: "Mon Apr 22 10:00:51 PDT 2019", color: "green", pokemon: { "name": "Tentacool", "number": 3 } },
+    { id: "3", name: "Fred", creation: "Mon Apr 22 10:00:51 PDT 2019", color: "green", pokemon: { "name": "Tentacool", "number": 3 } },
+    { id: "3", name: "Fred", creation: "Mon Apr 22 10:00:51 PDT 2019", color: "green", pokemon: { "name": "Tentacool", "number": 3 } },
+    { id: "3", name: "Fred", creation: "Mon Apr 22 10:00:51 PDT 2019", color: "green", pokemon: { "name": "Tentacool", "number": 3 } },
+    { id: "3", name: "Fred", creation: "Mon Apr 22 10:00:51 PDT 2019", color: "green", pokemon: { "name": "Tentacool", "number": 3 } },
+    { id: "3", name: "Fred", creation: "Mon Apr 22 10:00:51 PDT 2019", color: "green", pokemon: { "name": "Tentacool", "number": 3 } },
+    { id: "3", name: "Fred", creation: "Mon Apr 22 10:00:51 PDT 2019", color: "green", pokemon: { "name": "Tentacool", "number": 3 } },
+    { id: "3", name: "Fred", creation: "Mon Apr 22 10:00:51 PDT 2019", color: "green", pokemon: { "name": "Tentacool", "number": 3 } },
+    { id: "3", name: "Fred", creation: "Mon Apr 22 10:00:51 PDT 2019", color: "green", pokemon: { "name": "Tentacool", "number": 3 } },
+  ];
+
+
+
 
 }
