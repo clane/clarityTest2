daa70e70d9ae60266ede63cabea2301b8b5e60d6
Chris Lane <chlane@chlane-a01.vmware.com>
2019-05-20 15:18:49 -0700

    step skipping now works

--- clarity-test-app/src/app/wizards/wizard-skip-step/wizard-skip-step.component.ts
+++ clarity-test-app/src/app/wizards/wizard-skip-step/wizard-skip-step.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { ClrWizard } from '@clr/angular';
 
 @Component({
   selector: 'app-wizard-skip-step',
@@ -7,9 +9,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WizardSkipStepComponent implements OnInit {
 
-  constructor() { }
+
+  constructor(private titleService: Title) { }
+
+  componentTitle = 'Wizards - Wizard Skip Step';
+  setTitle(newTitle: string) { this.titleService.setTitle(newTitle); }
+  @ViewChild('componentHeading') elementToFocusOnInit:ElementRef;
+  @ViewChild('wizard') wizard: ClrWizard;
+  _open: boolean = false;
+
+
+  skipStepTwo: boolean = true;
+
+
+  toggleStepTwo() {
+      this.skipStepTwo = !this.skipStepTwo;
+  }
+
+  open() {
+      this._open = !this.open;
+  }
+
 
   ngOnInit() {
+    this.setTitle(this.componentTitle);
+    this.elementToFocusOnInit.nativeElement.focus();
   }
 
 }
