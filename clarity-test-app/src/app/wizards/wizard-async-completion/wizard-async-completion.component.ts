9f12ca5b3646d301df6ae5f47ed7a9ee614a0b95
Chris Lane <chlane@chlane-a01.vmware.com>
2019-05-22 09:40:47 -0700

    wizard-async-completion example now working

--- clarity-test-app/src/app/wizards/wizard-async-completion/wizard-async-completion.component.ts
+++ clarity-test-app/src/app/wizards/wizard-async-completion/wizard-async-completion.component.ts
@@ -9,16 +9,76 @@ import { ClrWizard } from '@clr/angular';
 })
 export class WizardAsyncCompletionComponent implements OnInit {
 
-  constructor() { }
-
+  
   constructor(private titleService: Title) { }
 
   componentTitle = 'Wizards - Wizard Async Completion';
   setTitle(newTitle: string) { this.titleService.setTitle(newTitle); }
   @ViewChild('componentHeading') elementToFocusOnInit:ElementRef;
+  @ViewChild("wizard") wizard: ClrWizard;
+  @ViewChild("myForm") formData: any;
+  @ViewChild("myFinishPage") finishPage: ClrWizardPage;
+
+  loadingFlag: boolean = false;
+  errorFlag: boolean = false;
+  checked = false;
+  finished = false;
+  open: boolean = false;
+  answer: number = null;
+
+  // have to define doCancel because page will prevent doCancel from working
+    // if the page had a previous button, you would need to call
+    // this.wizard.previous() manually as well...
+    doCancel(): void {
+      this.wizard.close();
+  }
+
+  get showCongrats(): boolean {
+    return !this.errorFlag && this.checked;
+  }
+
+  resetFinalPage(): void {
+    this.loadingFlag = false;
+    this.errorFlag = false;
+    this.checked = false;
+  }
+
+  goBack(): void {
+    this.wizard.previous();
+  }
+
+  doFinish(): void {
+    this.wizard.forceFinish();
+    this.resetFinalPage();
+  }
+
+  onCommit(): void {
+      let value: any = this.formData.value;
+      this.loadingFlag = true;
+      this.errorFlag = false;
+
+      if (this.finished) {
+        this.doFinish();
+        return;
+      }
+
+      setTimeout(() => {
+          if (value.answer === "42") {
+            this.finished = true;
+          } else {
+              this.finishPage.completed = false;
+              this.errorFlag = true;
+          }
+          this.checked = true;
+          this.loadingFlag = false;
+      }, 1000);
+  }
 
 
   ngOnInit() {
+    this.setTitle(this.componentTitle);
+    this.elementToFocusOnInit.nativeElement.focus();
   }
+
 
 }
