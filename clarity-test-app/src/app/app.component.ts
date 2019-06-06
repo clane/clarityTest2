8be9cd50c2dfdfbb58f8a312349c25b892f1371a
Christopher Lane <clane@Christophers-MacBook-Pro.local>
2019-06-06 09:20:43 -0700

    fixed build error

--- clarity-test-app/src/app/app.component.ts
+++ clarity-test-app/src/app/app.component.ts
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
 import { Component } from '@angular/core';
-import { RoutingSignalService } from './routing-signal.service';
-=======
 import { Component, ViewChild, ElementRef } from '@angular/core';
->>>>>>> 2284016089863a557969bff8b4a5f17e6756d8ff
 
 @Component({
   selector: 'app-root',
@@ -11,13 +7,8 @@ import { Component, ViewChild, ElementRef } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-<<<<<<< HEAD
 
-    constructor(private routingSignalService: RoutingSignalService) {}
-    title = 'clarity-test-app';
-    version = '8.0.0-beta.0';
 
-=======
   title = 'clarity-test-app';
   clrVersion = 'v2.0.0-rc.1';
   ngVersion = '8.0.0-rc.1';
@@ -30,5 +21,4 @@ export class AppComponent {
    
   
   }
->>>>>>> 2284016089863a557969bff8b4a5f17e6756d8ff
 }
