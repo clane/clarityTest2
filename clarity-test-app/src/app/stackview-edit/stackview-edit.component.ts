3ebc4b43f086b575cf154e7d5872bd822e2e7993
Chris Lane <chlane@chlane-a01.vmware.com>
2019-06-13 13:43:24 -0700

    stackview edit example now working

--- clarity-test-app/src/app/stackview-edit/stackview-edit.component.ts
+++ clarity-test-app/src/app/stackview-edit/stackview-edit.component.ts
@@ -14,6 +14,26 @@ export class StackviewEditComponent implements OnInit {
   setTitle(newTitle: string) { this.titleService.setTitle(newTitle); }
   @ViewChild('componentHeading') elementToFocusOnInit:ElementRef;
 
+    /*
+     * Modal edit demo
+     */
+    blocks: any[] = [
+      { title: 'Label 1', content: 'Content 1' },
+      {
+        title: 'Label 2',
+        content: 'Content 2',
+        children: [
+          { title: 'Sub-label 1', content: 'Sub-content 1' },
+          { title: 'Sub-label 2', content: 'Sub-content 2' },
+          { title: 'Sub-label 3', content: 'Sub-content 3' },
+        ],
+      },
+      { title: 'Label 3', content: 'Content 3' },
+    ];
+  
+    editModal: boolean = false;
+    
+
   ngOnInit() {
     this.setTitle(this.componentTitle);
     this.elementToFocusOnInit.nativeElement.focus();
