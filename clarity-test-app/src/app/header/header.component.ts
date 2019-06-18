3e602bf873e7498befe55bc9de00e09542de7ca9
Chris Lane <chlane@chlane-a01.vmware.com>
2019-06-18 10:02:17 -0700

    working on header example

--- clarity-test-app/src/app/header/header.component.ts
+++ clarity-test-app/src/app/header/header.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private titleService: Title) { }
+
+  componentTitle = 'Header Examples';
+  setTitle(newTitle: string) { this.titleService.setTitle(newTitle); }
+  @ViewChild('componentHeading') elementToFocusOnInit:ElementRef;
 
   ngOnInit() {
+    this.setTitle(this.componentTitle);
+    this.elementToFocusOnInit.nativeElement.focus();
   }
 
 }
